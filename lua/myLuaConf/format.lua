c8c5a824fcb743830928d7205bc6a977e9af4dea
dt <35782618+detour0@users.noreply.github.com>
2025-09-28 17:47:13 +0200

    feat: configured formating on save

--- lua/myLuaConf/format.lua
+++ lua/myLuaConf/format.lua
@@ -1,9 +1,15 @@
+local opts = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				}
+
 require("lze").load({
 	{
 		"conform.nvim",
 		for_cat = "format",
 		-- cmd = { "" },
-		-- event = "",
+		event = { "BufReadPre", "BufNewFile" },
 		-- ft = "",
 		keys = {
 			{ "<leader>FF", desc = "[F]ormat [F]ile" },
@@ -14,10 +20,6 @@ require("lze").load({
 
 			conform.setup({
 				formatters = {
-					-- goimports = {
-					-- 	command = "goimports",
-					-- 	args = { "-local", "gitlab.com/hmajid2301" },
-					-- },
 					yamlfmt = {
 						args = { "-formatter", "retain_line_breaks_single=true" },
 					},
@@ -41,14 +43,11 @@ require("lze").load({
 					nix = { "nixfmt" },
 					sql = { "sqlfluff" },
 				},
+        format_on_save = opts,
 			})
 
 			vim.keymap.set({ "n", "v" }, "<leader>FF", function()
-				conform.format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				})
+				conform.format(opts)
 			end, { desc = "[F]ormat [F]ile" })
 		end,
 	},
